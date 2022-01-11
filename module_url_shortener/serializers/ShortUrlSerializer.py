f9c391bfebd0128e8585b3ebcce47504ba509623
Danilo Santos <danilulp@hotmail.com>
2022-01-11 12:32:13 -0300

    updating logic + cleaning & fixes
    Updating logic from overwriting create class to validation, to keep validation all in one place;
    Updating how url is saved (custom_url saves only custom part, short_url saves the shortened url);
    Fixing errer accessing redirect url page from django admin;
    cleaning code

--- module_url_shortener/serializers/ShortUrlSerializer.py
+++ module_url_shortener/serializers/ShortUrlSerializer.py
@@ -1,13 +1,14 @@
-from datetime import date, timedelta
+import datetime
 
+from django.utils import timezone
 from environ import environ
-from rest_framework import serializers, status
-from rest_framework.response import Response
+from rest_framework import serializers
 
 from module_url_shortener.models.ShortUrl import ShortUrl
 from module_url_shortener.helper import create_random_code
 
 env = environ.Env()
+
 
 class ShortUrlSerializer(serializers.ModelSerializer):
 
@@ -18,35 +19,45 @@ class ShortUrlSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ('created', 'updated')
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.base_url = env('BASE_URL')
 
-    def create(self, validated_data):
-        if 'original_url' not in validated_data:
-            return Response("{'original_url': 'Not Found'}", status=status.HTTP_404_NOT_FOUND)
-        else:
-            self.create_shortened_url(validated_data)
+    def validate(self, attrs):
+        if not self.instance:
+            if 'original_url' not in attrs:
+                raise serializers.ValidationError({'original_url': 'Not Found'})
 
-    def create_shortened_url(self, validated_data):
-        if 'custom_url' in validated_data:
-            custom_url = '/u/{}'.format(validated_data['custom_url'])
+            if 'custom_url' in attrs:
+                custom_url = '/u/{}'.format(attrs['custom_url'].replace(" ", "_"))
+                q = self.Meta.model.objects.filter(custom_url=custom_url)
 
-            q = self.Meta.model.objects.filter(custom_url=custom_url)
-            if q.count() == 0:
-                custom_url = '/u/{}'.format(validated_data['custom_url'])
-                validated_data['short_url'] = custom_url
-        else:
-            if 'short_url' not in validated_data:
-                random_code = create_random_code()
-                short_url = '/u/{}'.format(random_code)
+                if q.count() > 0:
+                    if q.first().is_expired is False:
+                        raise serializers.ValidationError({'custom_url': 'Already in use'})
+                    if q.first().is_expired:
+                        q.first().delete()
+                else:
+                    attrs['short_url'] = '{}{}'.format(self.base_url, custom_url)
+                    attrs['custom_url'] = custom_url
 
-                if self.Meta.model.objects.filter(short_url=short_url).exists():
-                    return self.create_shortened_url(validated_data)
+            if 'short_url' not in attrs and 'custom_url' not in attrs:
+                short_url = self.create_short_url()
+                attrs['short_url'] = short_url
 
-                validated_data['short_url'] = short_url
-        if 'expiration' not in validated_data:
-            validated_data['expiration'] = date.today() + timedelta(days=7)
+            if 'expiration' in attrs:
+                if attrs['expiration'] < timezone.now():
+                    raise serializers.ValidationError({'expiration': 'Expiration can not be prior to today'})
+            else:
+                attrs['expiration'] = (timezone.now() + datetime.timedelta(days=7)).date()
 
-        new_short_url = super(ShortUrlSerializer, self).create(validated_data)
-        return new_short_url
+        return attrs
+
+    def create_short_url(self):
+        random_code = create_random_code()
+        short_url = '{}/u/{}'.format(self.base_url, random_code)
+
+        if self.Meta.model.objects.filter(short_url=short_url).exists():
+            return self.create_short_url()
+
+        return short_url
--- module_url_shortener/views/RedirectViewset.py
+++ module_url_shortener/views/RedirectViewset.py
@@ -1,11 +1,14 @@
 from django.shortcuts import redirect
-from rest_framework import viewsets, mixins, status
+from environ import environ
+from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from module_url_shortener.models.ShortUrl import ShortUrl
 from module_url_shortener.serializers.ShortUrlSerializer import ShortUrlSerializer
+
+env = environ.Env()
 
 
 class RedirectViewset(APIView):
@@ -15,8 +18,9 @@ class RedirectViewset(APIView):
     ordering = '-id'
 
     def get(self, request, *args, **kwargs):
-        url = request.get_full_path()
-        short_url = ShortUrl.objects.filter(short_url=url)
+        url_path = request.get_full_path()
+        short_link = '{}{}'.format(env('BASE_URL'), url_path)
+        short_url = ShortUrl.objects.filter(short_url=short_link)
 
         if short_url.count() == 0:
             return Response({"detail": "Not found."}, status=status.HTTP_404_NOT_FOUND)
